2f3b4a575b039d6f32f573a3396e1d44a96a0025
Vidushi-P <230190.cd@rmkec.ac.in>
2024-12-08 18:38:11 +0530

    Reapply "Reapply "UI and scripting for the API""
    This reverts commit 08ee22330f68dabfd40cdd0a405b0117b87fba52.

---
+++ DrishyaFinal/Assets/UIManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using SFB; 
+
+public class UIManager : MonoBehaviour
+{
+    public Button uploadButton;
+    public Button convertButton;
+    public BlenderIntegration blenderIntegration;
+    private string uploadedImagePath;
+
+    void Start()
+    {
+        uploadButton.onClick.AddListener(UploadFloorplan);
+        convertButton.onClick.AddListener(ConvertFloorplanTo3D);
+    }
+
+    void UploadFloorplan()
+    {
+        var extensions = new[] {
+        new ExtensionFilter("Image Files", "png", "jpg", "jpeg")
+    };
+
+   
+    string[] paths = StandaloneFileBrowser.OpenFilePanel("Select Floorplan Image", "", extensions, false);
+    
+    if (paths.Length > 0)
+    {
+        uploadedImagePath = paths[0]; 
+        Debug.Log("Selected file: " + uploadedImagePath); 
+    }
+    else
+    {
+        Debug.Log("No file selected.");
+    }
+    }
+
+    void ConvertFloorplanTo3D()
+    {
+        if (!string.IsNullOrEmpty(uploadedImagePath))
+        {
+            blenderIntegration.ConvertTo3D(uploadedImagePath);
+        }
+        else
+        {
+            Debug.Log("No image uploaded.");
+        }
+    }
+}
